9601edf0daaffa590fe9cf4744bc0146404d3c04
paul lovato <lovato.paul97@yahoo.com>
2022-11-07 04:03:17 -0500

    delete notes updated

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,3 @@
-// api routes
 
 // dependencies 
 const path = require('path');
@@ -33,11 +32,20 @@ module.exports = (app) => {
     db.push(userNote);
     fs.writeFileSync('db/db.json', JSON.stringify(db));
     res.json(db);
-    
+
   });
 
-  // to delete notes
-  // app.post('/notes/:id', (req, res) => {
-  //   res.sendFile(path.join(__dirname, '../db/db.json'));
-  // })
+
+  // DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete.
+  app.delete('/api/notes/:id', (req, res) => {
+    // reading notes form db.json
+    let db = JSON.parse(fs.readFileSync('db/db.json'))
+    // removing note with id
+    let deleteNotes = db.filter(item => item.id !== req.params.id);
+    console.log(req.params.id);
+    // Rewriting note to db.json
+    fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
+    res.json(deleteNotes);
+    
+  })
 };
